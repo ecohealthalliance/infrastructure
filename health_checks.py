5f22995c17739393094eae4487905fe47e021663
Freddie Rosario <rosario@ecohealthalliance.org>
2015-09-15 15:26:57 -0400

    Adding health_checks py script

---
+++ health_checks.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Name: health_check.py
+# Purpose: Script to run as an automated healt check for our websites
+# Author: Freddie Rosario <rosario@ecohealthalliance.org>
+
+import requests
+from sys import exit
+failure = False
+
+urls = [
+    'https://mantle.ecohealthalliance.org/grrs',
+    'http://www.ecohealthalliance.org/',
+    'https://eidr.ecohealthalliance.org/',
+    'https://tater-dev.ecohealth.io/',
+    'https://tater.ecohealthalliance.org/',
+    'https://grits.ecohealthalliance.org/',
+    'https://grits-dev.ecohealth.io/'
+  ]
+
+for u in urls:
+  try:
+    r = requests.get(u,verify=False)
+    if r.status_code != 200:
+      print "Problem with: " + u
+      failure = True
+
+  except requests.exceptions.SSLError:
+    print "SSL verification failed for: " + u
+    failure = True
+  
+  except requests.exceptions.ConnectionError:
+    print "Could not connect to: " + u
+    failure = True
+
+if failure:
+  exit(1)
+else:
+  print "All monitored websites up!"
+
