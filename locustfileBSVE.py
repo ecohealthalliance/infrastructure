f9f76fcaee03a8f462fed8b17508ef76e76357b4
Freddie Rosario <rosario@ecohealthalliance.org>
2015-10-13 16:39:18 -0400

    Added locustfileBSVE.py

---
+++ locustfileBSVE.py
@@ -0,0 +1,17 @@
+# Name: locustfileBSVE.py
+# Purpose: Use locust tool to load test the BSVE grits app
+# Author: Freddie Rosario <rosario@ecohealthalliance.org>
+# Usage: locust -f locustfileBSVE.py --host=https://grits-dev.ecohealthalliance.org
+#        Visit http://localhost:8089 to see the webui after that
+
+from locust import HttpLocust, TaskSet, task
+
+class LoadTestTasks(TaskSet):
+  @task
+  def new(l):
+    l.client.get("/new?compact=true&bsveAccessKey=loremipsumhello714902&hideBackButton=true",verify=False)
+
+
+class User(HttpLocust):
+  task_set = LoadTestTasks
+
