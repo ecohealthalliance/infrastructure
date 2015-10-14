9bf38cce484ddc1b53551435132cfa172b96fc69
Freddie Rosario <rosario@ecohealthalliance.org>
2015-10-14 11:04:29 -0400

    Update to locustfile, also adding gitignore file

--- locustfileBSVE.py
+++ locustfileBSVE.py
@@ -9,7 +9,11 @@ from locust import HttpLocust, TaskSet, task
 class LoadTestTasks(TaskSet):
   @task
   def new(l):
-    l.client.get("/new?compact=true&bsveAccessKey=loremipsumhello714902&hideBackButton=true",verify=False)
+    l.client.get("/new?compact=true&bsveAccessKey=loremipsumhello714902&hideBackButton=true", verify=False)
+
+  @task
+  def diagnose(l):
+    l.client.post('/api/v1/public_diagnose', data={'api_key': 'grits28754', 'content': 'loadtest'}, verify=False)
 
 
 class User(HttpLocust):
