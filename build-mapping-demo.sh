03d5a8d37176ede9944d296e2261adb8c59883d0
Freddie Rosario <rosario@ecohealthalliance.org>
2015-10-16 12:13:44 -0400

    Adding build-mapping-demo.sh

---
+++ build-mapping-demo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Name: build-mapping-demo.sh
+#Purpose: Shell script that jenkins will use to build docker container to demo mapping engine build by DIT
+#Author: Freddie Rosario <rosario@ecohealthalliance.org>
+
+#Checkout necessary repos
+mkdir -p example/packages
+cd example/packages
+git clone git@github.com:ecohealthalliance/grits-net-meteor.git
+git clone git@github.com:ecohealthalliance/grits-net-mapper.git
+
+
+#Build the meteor app
+cd ../
+source /var/lib/jenkins/.profile 
+nvm use v0.12.7
+rm -fr ./gritsbuild
+/opt/meteor/meteor build ./gritsbuild --directory || exit 1
+
+
+#Create the docker file
+touch ./gritsbuild/bundle/Dockerfile
+cat > ./gritsbuild/bundle/Dockerfile<<-EOF
+FROM node:0.12
+EXPOSE 80
+ADD . .
+RUN cd programs/server && npm install
+CMD node main.js
+EOF
+
+#Build the example app container
+docker build -t grits-net-meteor ./gritsbuild/bundle/
