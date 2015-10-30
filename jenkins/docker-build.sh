805d3f7e59db3ed0a81265d1c262ea4ad150fda7
Freddie Rosario <rosario@ecohealthalliance.org>
2015-10-30 15:20:35 -0400

    Added docker-build.sh

---
+++ jenkins/docker-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Name: docker-build.sh
+# Purpose: Script to run on jenkins to build and publish docker images
+# Author: Freddie Rosario <rosario@ecohealthalliance.org>
+
+#Argument parsing
+if [[ $# > 1 ]]
+then
+  key="$1"
+fi
+
+case $key in
+  --image-name)
+    IMAGE_NAME="$2"
+    ;;
+
+  *)
+    echo "Please specify hostname like so: --image-name docker-repository.tater.io/<repo name>/<image name>"
+    echo
+    echo
+    exit 1
+    ;;
+esac
+
+
+DATE=$(date +%F) &&\
+docker build -t $IMAGE_NAME:$DATE . &&\
+docker tag -f $IMAGE_NAME:$DATE $IMAGE_NAME:latest &&\
+docker push $IMAGE_NAME
+
