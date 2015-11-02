6bdc2c81a96578f8d9f2dc87dbd1843fa744972e
Freddie Rosario <rosario@ecohealthalliance.org>
2015-11-02 16:07:06 -0500

    Adding docker-clean-orphans.sh

---
+++ jenkins/docker-clean-orphans.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Name: docker-clean-orphans.sh
+# Purpose: Remove docker images labeled as "none"
+# Author: Freddie Rosario <rosario@ecohealthalliance.org>
+
+docker images|grep none|awk '{print $3}'|xargs docker rmi -f
