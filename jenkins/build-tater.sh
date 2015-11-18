9029aa6a385e6562bd7e5d28bfea008b7eae0b8c
Freddie Rosario <rosario@ecohealthalliance.org>
2015-11-18 12:25:29 -0500

    Changes to support new eha docker container

---
+++ jenkins/build-tater.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Name: build-tater.sh
+#Purpose: Compile tater into binaries
+#Author: Freddie Rosario <rosario@ecohealthalliance.org>
+
+meteor build build --directory --architecture os.linux.x86_64
