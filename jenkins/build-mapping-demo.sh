bbc6f8dc31f943311441e145aaaa39a2a9b4fd32
Freddie Rosario <rosario@ecohealthalliance.org>
2015-11-19 12:19:30 -0500

    Update to build-mapping-demo.sh to support flight data import process

--- jenkins/build-mapping-demo.sh
+++ jenkins/build-mapping-demo.sh
@@ -36,19 +36,22 @@ EOF
 #Create run.sh
 touch ./gritsbuild/bundle/run.sh
 cat > ./gritsbuild/bundle/run.sh<<-EOF
-virtualenv grits-net-consume-env &&\
-source grits-net-consume-env/bin/activate &&\
+virtualenv grits-net-consume-env &&\ 
+source grits-net-consume-env/bin/activate &&\ 
 pip install -r requirements.txt
 
+sed -i 's/url-innovata.com/suwweb03.innovata-llc.com/' grits_ftp_config.py
+sed -i 's/username/ECOHEALTH/' grits_ftp_config.py
+sed -i 's/password/CC6832Hy/' grits_ftp_config.py
+
 python grits_flight_pull.py
-python grits_consume.py --type DiioAirport tests/data/MiExpressAllAirportCodes.tsv
-python grits_consume.py --type FlightGlobal tests/data/GlobalDirectsSample_20150728.csv
+python grits_consume.py --type FlightGlobal -m 10.0.0.175 -d grits-net-meteor /data/EcoHealth_*.csv
 
 node main.js
 EOF
 
 #Build the example app container
-cp -R ./packages/grits-net-consume/*.py ./packages/grits-net-consume/requirements.txt ./gritsbuild/bundle/
+cp -R ./packages/grits-net-consume/* ./gritsbuild/bundle/
 docker build -t grits-net-meteor ./gritsbuild/bundle/
 
 
