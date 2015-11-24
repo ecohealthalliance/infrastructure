6d557d5dcb7ea22c38933041f0c28825ab031059
Freddie Rosario <rosario@ecohealthalliance.org>
2015-11-24 09:46:46 -0500

    Updated deploy-flirt.sh

--- jenkins/deploy-flirt.sh
+++ jenkins/deploy-flirt.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 #Dump the freshly built image into a tarball
-docker save grits-net-meteor > /tmp/grits-net-meteor.tar
-echo "Docker image exported"
-gzip -9 /tmp/grits-net-meteor.tar 
-"Exported image now compressed"
+/bin/rm /tmp/grits-net-meteor.tar.gz
+/usr/bin/docker save grits-net-meteor > /tmp/grits-net-meteor.tar
+/bin/echo "Docker image exported"
+/bin/gzip -9 /tmp/grits-net-meteor.tar 
+/bin/echo "Exported image now compressed"
 
 #Import image on demo box
-scp -i /keys/infrastructure.pem /tmp/grits-net-meteor.tar.gz  ubuntu@52.23.65.236:/tmp/
-ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 gzip -d /tmp/grits-net-meteor.tar.gz
-ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 "sudo docker load < /tmp/grits-net-meteor.tar"
-"Image now imported on demo box"
+/usr/bin/scp -i /keys/infrastructure.pem /tmp/grits-net-meteor.tar.gz  ubuntu@52.23.65.236:/tmp/
+/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 /bin/gzip -d /tmp/grits-net-meteor.tar.gz
+/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 "/usr/bin/sudo /usr/bin/docker load < /tmp/grits-net-meteor.tar"
+/bin/echo "Image now imported on demo box"
 
 #Clean up old data
-rm /tmp/grits-net-meteor.tar.gz
-ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 rm /tmp/grits-net-meteor.tar
+/bin/rm /tmp/grits-net-meteor.tar.gz
+/usr/bin/ssh -i /keys/infrastructure.pem ubuntu@52.23.65.236 /bin/rm /tmp/grits-net-meteor.tar
