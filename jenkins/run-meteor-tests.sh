75e06dbc04e7ed215fc8f4bde5326e3d949b6b0f
Freddie Rosario <rosario@ecohealthalliance.org>
2015-12-01 17:12:44 -0500

    Updated run-meteor-tests jenkins script to depend on Xvnc plugin instead

--- jenkins/run-meteor-tests.sh
+++ jenkins/run-meteor-tests.sh
@@ -5,8 +5,5 @@
 
 java -version
 source ~/.profile && nvm use v0.12.7
-export DISPLAY=:99.0
-Xvfb :99 -screen 0 1600x1200x16 &
-sleep 10
 VELOCITY_CI=1 CHIMP_OPTIONS="--browser=chrome --no-screenshotsOnError" meteor --test
 
