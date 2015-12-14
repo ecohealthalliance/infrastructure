3276ef50c4e0e5c1245929c59216e65ab188eabc
Daniel Sullivan <danielalexandersullivan@gmail.com>
2015-12-14 14:33:22 -0500

    Pass instance name as argument into deploy tater script

--- jenkins/deploy-tater.sh
+++ jenkins/deploy-tater.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+#Argument parsing
+key="$1"
+
+case $key in
+  --instance-name)
+    INSTANCE_NAME="$2"
+    ;;
+
+  *)
+    echo "Please specify instance name, e.g.: --instance-name eha"
+    echo
+    echo
+    exit 1
+    ;;
+esac
+
 scp -i /keys/infrastructure.pem docker/containers/eha ubuntu@po.tater.io:/tmp
 export remote_ssh="ssh -i /keys/infrastructure.pem ubuntu@po.tater.io"
 
-$remote_ssh "sudo docker-compose -f /tmp/eha pull"
-id=$($remote_ssh '$(sudo docker ps|grep eha| sed "s/ .*//")')
+$remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME pull"
+export id="$($remote_ssh sudo docker ps | grep $INSTANCE_NAME | sed 's/ .*//')"
 $remote_ssh "sudo docker stop $id"
 $remote_ssh "sudo docker rm $id"
-$remote_ssh "sudo docker-compose -f /tmp/eha up -d"
+$remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME up -d"
