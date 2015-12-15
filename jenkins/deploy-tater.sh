bd7332d160c6a1f5578881c7eef981a87a80756a
Daniel Sullivan <danielalexandersullivan@gmail.com>
2015-12-15 11:47:57 -0500

    Deploy to all tater instances at once

--- jenkins/deploy-tater.sh
+++ jenkins/deploy-tater.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#Argument parsing
-key="$1"
+deploy_instance () {
+  INSTANCE_NAME="$1"
+  echo
+  echo
+  echo "#########################"
+  echo "Deploying $INSTANCE_NAME:"
+  echo
 
-case $key in
-  --instance-name)
-    INSTANCE_NAME="$2"
-    ;;
+  scp -i /keys/infrastructure.pem docker/containers/tater/$INSTANCE_NAME ubuntu@po.tater.io:/tmp
+  export remote_ssh="ssh -i /keys/infrastructure.pem ubuntu@po.tater.io"
 
-  *)
-    echo "Please specify instance name, e.g.: --instance-name eha"
-    echo
-    echo
-    exit 1
-    ;;
-esac
+  $remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME pull"
+  export id="$($remote_ssh sudo docker ps | grep $INSTANCE_NAME | sed 's/ .*//')"
+  $remote_ssh "sudo docker stop $id"
+  $remote_ssh "sudo docker rm $id"
+  $remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME up -d"
+  return 0
+}
 
-scp -i /keys/infrastructure.pem docker/containers/tater/$INSTANCE_NAME ubuntu@po.tater.io:/tmp
-export remote_ssh="ssh -i /keys/infrastructure.pem ubuntu@po.tater.io"
-
-$remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME pull"
-export id="$($remote_ssh sudo docker ps | grep $INSTANCE_NAME | sed 's/ .*//')"
-$remote_ssh "sudo docker stop $id"
-$remote_ssh "sudo docker rm $id"
-$remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME up -d"
+FILES=docker/containers/tater/*
+for file in $FILES
+do
+  deploy_instance ${file##*/}
+done
