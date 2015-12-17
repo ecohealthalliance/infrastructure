bbe3201bfd30b24d2a4f904b16f5a63f9062fca5
sturmer-eha <sturmer@ecohealthalliance.org>
2015-12-17 18:25:03 -0500

    Automatic port assignment & --port argument for the docker container generator shell script

--- docker/containers/generate-new-tater-container.sh
+++ docker/containers/generate-new-tater-container.sh
@@ -1,38 +1,84 @@
 #!/bin/bash
 
+# Help function
+function usage() {
+  echo "Usage: $0 --name <eha> [--port <8002>]" 1>&2
+}
+
+# Returns 1 if the provided port is free
+function probePort() {
+  local port=$1
+  nc 127.0.0.1 $port < /dev/null
+  echo $?
+}
+
+BAD_ARGS=""
+
 #Argument parsing
-if [[ $# > 1 ]]
+while [ "$1" ]
+do
+  case "$1" in
+   -n|--name)
+    NAME="$2"
+    shift 2
+    ;;
+   -p|--port)
+    PORT="$2"
+    shift 2
+    ;;
+   *)
+    BAD_ARGS="$BAD_ARGS $1"
+    shift
+    ;;
+  esac
+done
+
+if [[ "$BAD_ARGS" ]]
 then
-  key="$1"
+	usage
+	echo "Bad arguments: $BAD_ARGS"
+  exit 1
 fi
 
-case $key in
-  -n|--name)
-    NAME="$2"
-    ;;
-  *)
-    echo "Please specify container name, e.g.: --name eha"
-    echo ""
-    exit 1;
-    ;;
-esac
+if [[ "$PORT" ]]
+then
+  if [[ "$(probePort $PORT)" < 1 ]]
+  then
+    echo "Port $PORT is in use, please specify a different port"
+    exit 1
+  fi
+else
+  PORT_START=8002
+  PORT_END=65535
+
+  for i in $(seq $PORT_START $PORT_END);
+  do
+    if [[ "$(probePort $i)" > 0 ]]
+    then
+      PORT=$i
+      break
+    fi
+  done
+fi
 
 # Lowercase the $NAME
-NAME="$(tr [A-Z] [a-z] <<< "$NAME")"
+NAME="$(tr [A-Z] [a-z] <<< $NAME)"
 
-# Create the sub-directory
-mkdir -p tater
+if [[ "$NAME" ]]
+then
+  # Create the sub-directory
+  mkdir -p tater
 
-if [[ -n "$NAME" ]]; then
-  echo "${NAME}.tater.io:
-container_name: ${NAME}.tater.io
+  # Ouput the new container file
+  echo "$NAME.tater.io:
+container_name: $NAME.tater.io
 image: docker-repository.tater.io:5000/apps/tater
 ports:
-  - \"8002:3000\"
+  - \"$PORT:3000\"
 restart: always
 environment:
-  - MONGO_URL=mongodb://10.0.0.92:27017/${NAME}
-  - ROOT_URL=https://${NAME}.tater.io
+  - MONGO_URL=mongodb://10.0.0.92:27017/$NAME
+  - ROOT_URL=https://$NAME.tater.io
   - PORT=3000
-" >> "tater/${NAME}"
+" > "tater/$NAME"
 fi
