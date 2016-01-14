eeeaa0ebd16c2a3cd50120c169a84e629b6cf9f5
Freddie Rosario <rosario@ecohealthalliance.org>
2016-01-14 18:40:20 -0500

    Update to provision script

--- jenkins/provision-new-tater-instance.sh
+++ jenkins/provision-new-tater-instance.sh
@@ -1,3 +1,7 @@
+#This script is meant to run inside of a jenkins job
+#This script also assumes that the infrastructure repo is checked out
+#Another assumption is that we are executing this script from the top level of the infrastructure repo
+
 #Make sure we have pyhaproxy ready to go
 [ -d "pyhaproxy" ] || git clone https://github.com/frosario/pyhaproxy.git
 export PYTHONPATH="$(pwd)/pyhaproxy"
@@ -13,6 +17,6 @@ $WORKSPACE/docker/images/app-router/update_config.py \
 #Commit update to revision control
 git add -A
 git commit -m "New tater instance: $instance_name   $BUILD_URL"
-git push
-
+#HEAD_ON_BRANCH=$(echo $GIT_BRANCH|awk -F"/" '{print $1 " HEAD:"$2}')
+#git push $HEAD_ON_BRANCH
 
