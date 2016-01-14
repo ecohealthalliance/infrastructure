177b109bf13d5b0562a5e8953be13b4f2ff0b616
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-01-14 17:44:56 -0500

    Update mantle CI script

--- jenkins/run-mantle-tests.sh
+++ jenkins/run-mantle-tests.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-killall -9 node
+kill `lsof -t -i:3000`
 export NVM_DIR="/var/lib/jenkins/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm use v5.2.0
+
 meteor & 
-CUCUMBER_TAIL=1 ~/node_modules/.bin/chimp --ddp=http://localhost:3000 --path=tests/cucumber/features/ --coffee=true --chai=true --sync=false
-killall -9 node
+tail -f testoutput.txt &
+
+CUCUMBER_TAIL=1 ~/node_modules/.bin/chimp --ddp=http://localhost:3000 --path=tests/cucumber/features/ --coffee=true --chai=true --sync=false > testoutput.txt
+
+kill `lsof -t -i:3000`
+if grep -q "failed steps" testoutput.txt
+then
+  echo "Tests Failed"
+  exit 1
+fi
+echo "Tests Passed"
+exit 0
