57f8055977e781bd27781a484287f5a67500cc45
Freddie Rosario <rosario@ecohealthalliance.org>
2016-01-26 12:52:53 -0500

    updated deploy-flirt.sh script

--- jenkins/deploy-flirt.sh
+++ jenkins/deploy-flirt.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #Dump the freshly built image into a tarball
-/bin/rm /tmp/grits-net-meteor.tar.gz
-/usr/bin/docker save grits-net-meteor > /tmp/grits-net-meteor.tar
+/bin/rm /tmp/flirt.tar.gz
+/usr/bin/docker save flirt > /tmp/flirt.tar
 /bin/echo "Docker image exported"
-/bin/gzip -9 /tmp/grits-net-meteor.tar 
+/bin/gzip -1 /tmp/flirt.tar 
 /bin/echo "Exported image now compressed"
 
 #Import image on demo box
-/usr/bin/scp -i /keys/infrastructure.pem /tmp/grits-net-meteor.tar.gz  ubuntu@52.23.65.236:/tmp/
-/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 /bin/gzip -d /tmp/grits-net-meteor.tar.gz
-/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 "/usr/bin/sudo /usr/bin/docker load < /tmp/grits-net-meteor.tar"
+/usr/bin/scp -i /keys/infrastructure.pem /tmp/flirt.tar.gz  ubuntu@52.23.65.236:/tmp/
+/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 /bin/gzip -d /tmp/flirt.tar.gz
+/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 "/usr/bin/sudo /usr/bin/docker load < /tmp/flirt.tar"
 /bin/echo "Image now imported on demo box"
 
 #Clean up old data
-/bin/rm /tmp/grits-net-meteor.tar.gz
-/usr/bin/ssh -i /keys/infrastructure.pem ubuntu@52.23.65.236 /bin/rm /tmp/grits-net-meteor.tar
+/bin/rm /tmp/flirt.tar.gz
+/usr/bin/ssh -i /keys/infrastructure.pem ubuntu@52.23.65.236 /bin/rm /tmp/flirt.tar
