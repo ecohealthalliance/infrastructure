2b1f699a4a750c6197397d332374ef3852fee8df
Freddie Rosario <rosario@ecohealthalliance.org>
2016-01-27 10:35:10 -0500

    Update to deploy-tater.sh

--- jenkins/deploy-tater.sh
+++ jenkins/deploy-tater.sh
@@ -13,9 +13,9 @@ deploy_instance () {
 
   $remote_ssh "sudo --login docker-compose -f /tmp/$INSTANCE_NAME pull"
   export id="$($remote_ssh sudo docker ps | grep $INSTANCE_NAME | sed 's/ .*//')"
-  $remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME stop $id"
-  $remote_ssh "sudo docker-compose -f  /tmp/$INSTANCE_NAME rm $id"
-  $remote_ssh "sudo docker-compose -f /tmp/$INSTANCE_NAME up -d"
+  $remote_ssh "sudo --login docker-compose -f /tmp/$INSTANCE_NAME stop $id"
+  $remote_ssh "sudo --login docker-compose -f  /tmp/$INSTANCE_NAME rm $id"
+  $remote_ssh "sudo --login docker-compose -f /tmp/$INSTANCE_NAME up -d"
   return 0
 }
 
