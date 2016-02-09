0dbca8e17d80cbc6f80ac6a782b4149f60346a1f
Freddie Rosario <frosario@users.noreply.github.com>
2016-02-09 13:59:16 -0500

    Update tornado.sh

--- docker/images/flirt/tornado.sh
+++ docker/images/flirt/tornado.sh
@@ -2,4 +2,4 @@
 
 cd /flirt-simulation-service
 source /flirt-simulation-service-env/bin/activate && \
-python server.py port 45000 --mongo_host=10.0.0.175 --mongo_port=27017 mongo_database grits-net-meteor
+python server.py port 45000 --mongo_host=10.0.0.175 --mongo_port=27017 --mongo_database=grits-net-meteor
