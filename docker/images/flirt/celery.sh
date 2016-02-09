9e6b476006486f4ae6502036b7a6a78be9910796
Freddie Rosario <rosario@ecohealthalliance.org>
2016-02-09 10:11:18 -0500

    Virtual env fix

--- docker/images/flirt/celery.sh
+++ docker/images/flirt/celery.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /flirt-simulation-service/simulator/
-source /flirt-simulation-service-env/bin/activate && \
+cd /flirt-simulation-service
+virtualenv /flirt-simulation-service-env &&\
+source /flirt-simulation-service-env/bin/activate &&\
+pip install -r requirements.txt &&\
 celery worker -A tasks --loglevel=INFO --concurrency=2
 
--- docker/images/flirt/tornado.sh
+++ docker/images/flirt/tornado.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
 cd /flirt-simulation-service
-source /flirt-simulation-service-env/bin/activate && \
+virtualenv /flirt-simulation-service-env &&\
+source /flirt-simulation-service-env/bin/activate &&\
+pip install -r requirements.txt &&\
 python server.py port 45000 mongo_host 10.0.0.175 mongo_port 27017 mongo_database grits-net-meteor
