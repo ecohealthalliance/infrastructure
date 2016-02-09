e37fd4dd5467f1082b83a22cddb552075d586a1d
Freddie Rosario <rosario@ecohealthalliance.org>
2016-02-09 11:07:08 -0500

    Revert "Virtual env fix"
    This reverts commit 9e6b476006486f4ae6502036b7a6a78be9910796.

--- docker/images/flirt/celery.sh
+++ docker/images/flirt/celery.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /flirt-simulation-service
-virtualenv /flirt-simulation-service-env &&\
-source /flirt-simulation-service-env/bin/activate &&\
-pip install -r requirements.txt &&\
+cd /flirt-simulation-service/simulator/
+source /flirt-simulation-service-env/bin/activate && \
 celery worker -A tasks --loglevel=INFO --concurrency=2
 
--- docker/images/flirt/tornado.sh
+++ docker/images/flirt/tornado.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+
 cd /flirt-simulation-service
-virtualenv /flirt-simulation-service-env &&\
-source /flirt-simulation-service-env/bin/activate &&\
-pip install -r requirements.txt &&\
+source /flirt-simulation-service-env/bin/activate && \
 python server.py port 45000 mongo_host 10.0.0.175 mongo_port 27017 mongo_database grits-net-meteor
