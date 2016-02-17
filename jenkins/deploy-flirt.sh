8aec223d523498ac3bc03cfe56116a062909d220
Freddie Rosario <rosario@ecohealthalliance.org>
2016-02-17 10:35:43 -0500

    Refactored deploy-flirt.sh

--- jenkins/deploy-flirt.sh
+++ jenkins/deploy-flirt.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Note: This script is intended for jenkins.
+#Please run this from the top level of the infrastructure repo
 
 #Dump the freshly built image into a tarball
 /bin/rm /tmp/flirt.tar.gz
@@ -7,12 +9,21 @@
 /bin/gzip -1 /tmp/flirt.tar 
 /bin/echo "Exported image now compressed"
 
+#Useful alias/function
+export ssh_command="/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236"
+function scp_file { /usr/bin/scp -i /keys/infrastructurea.pem $1 ubuntu@52.23.65.236:/tmp/ }
+
 #Import image on demo box
-/usr/bin/scp -i /keys/infrastructure.pem /tmp/flirt.tar.gz  ubuntu@52.23.65.236:/tmp/
-/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 /bin/gzip -d /tmp/flirt.tar.gz
-/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@52.23.65.236 "/usr/bin/sudo /usr/bin/docker load < /tmp/flirt.tar"
+scp_file /tmp/flirt.tar.gz
+$ssh_command /bin/gzip -d /tmp/flirt.tar.gz
+$ssh_command "/usr/bin/sudo /usr/bin/docker load < /tmp/flirt.tar"
 /bin/echo "Image now imported on demo box"
 
 #Clean up old data
 /bin/rm /tmp/flirt.tar.gz
-/usr/bin/ssh -i /keys/infrastructure.pem ubuntu@52.23.65.236 /bin/rm /tmp/flirt.tar
+$ssh_command /bin/rm /tmp/flirt.tar
+
+#Reprovision flirt container
+scp_file docker/containers/flirt.yml
+$ssh_command "sudo docker-compose -f /tmp/flirt.yml up -d"
+
