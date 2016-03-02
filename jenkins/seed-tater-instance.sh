3f8d283aebd644d4cec0531e18c08718d7d9a4ec
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-03-02 12:12:49 -0500

    Add jenkins script for seeding tater instance

---
+++ jenkins/seed-tater-instance.sh
@@ -0,0 +1,11 @@
+#Wait for DNS
+ping=`host $instance_name.tater.io 8.8.8.8 | grep -o "not found"`
+while [ "$ping" ]
+do
+  echo $ping
+  sleep 60
+  ping=`host $instance_name.tater.io 8.8.8.8 | grep -o "not found"`
+done
+
+#Seed database
+phantomjs --ssl-protocol=any --web-security=false --ignore-ssl-errors=true jenkins/seedDatabase.js "http://$instance_name.tater.io/seed?fullName=$full_name&emailAddress=$email_address&orgName=$organization_name&tenantName=$instance_name&stripeCustomerId=$stripe_customer_id"
