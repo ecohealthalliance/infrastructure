c55852f5db0678c041d2ae9c4f8a8c8b6998f030
arnoldbm <arnold@ecohealthalliance.org>
2016-03-08 10:44:14 -0500

    Docker updates for spa

--- docker/images/spa/run.sh
+++ docker/images/spa/run.sh
@@ -1,8 +1,8 @@
-# Default run.sh..
+#!/bin/bash
 
 #Import heat map data
 aws s3 sync s3://promed-database/ ./
-mongorestore -h 10.0.0.175 -d promed -c blindspots ./dump/promed/blindspots.bson
+mongorestore -h 172.30.2.160 -d promed -c blindspots ./dump/promed/blindspots.bson
 
 #Start the app
 service supervisor start
