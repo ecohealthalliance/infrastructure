b12c8493a037e224693c6352ecc2f70cc37de43b
Freddie Rosario <rosario@ecohealthalliance.org>
2016-04-14 10:44:31 -0400

    update to build-era shell script

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -2,6 +2,6 @@
 #This is a jenkins script
 
 git checkout master
-git submodule foreach git pull origin master
+git submodule update --init
 docker build -t era /opt/infrastructure/docker/images/era
 
