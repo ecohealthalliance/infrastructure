65c5e9434ab45214f5517862feb0388453c5199e
Freddie Rosario <rosario@ecohealthalliance.org>
2016-05-02 12:06:40 -0400

    Update to parse dashboard

--- docker/images/parse-server/parse-dashboard.sh
+++ docker/images/parse-server/parse-dashboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-parse-dashboard --appId testapp --masterKey testkey --serverURL $SERVER_URL
+parse-dashboard --appId testapp --masterKey testkey --serverURL $SERVER_URL --allowInsecureHTTP=1
 
