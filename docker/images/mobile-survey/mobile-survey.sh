3c9f1352f1b1ca743746704bc31d209aec6e62d4
Freddie Rosario <rosario@ecohealthalliance.org>
2016-06-01 11:10:05 -0400

    Change away from meteor build to meteor run --production

--- docker/images/mobile-survey/mobile-survey.sh
+++ docker/images/mobile-survey/mobile-survey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export METEOR_SETTINGS=$(cat /shared/settings-production.json)
-meteor node /mobile-survey-webapp/app/build/bundle/main.js 
+cd /mobile-survey-webapp/app &&\
+meteor run --settings /shared/settings-production.json --production
 
