ca64302d623ee5773df41d48ebf7ce49bce1897d
Freddie Rosario <rosario@ecohealthalliance.org>
2016-06-16 12:15:08 -0400

    Another mongodb run.sh update

--- docker/images/mongodb/run.sh
+++ docker/images/mongodb/run.sh
@@ -9,10 +9,10 @@ then
   echo "Failing because we do not want to write files inside of a container."
   exit 1
 else
-  mkdir /var/lib/mongodb || true
+  (mkdir /var/lib/mongodb || true)
   chown mongodb:mongodb /var/lib/mongodb/
 
-  mkdir /var/log/mongodb || true
+  (mkdir /var/log/mongodb || true)
   touch /var/log/mongodb/mongodb.log
   chown -R mongodb:mongodb /var/log/mongodb/
   /usr/bin/mongod --config /etc/mongodb.conf
