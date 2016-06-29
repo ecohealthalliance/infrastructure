b695813ecafd41d171fcec369836aa4bd1e68887
Freddie Rosario <rosario@ecohealthalliance.org>
2016-06-29 17:22:45 -0400

    Trying Yuri's suggestion of going back to a modern node and npm version

--- docker/images/mobile-survey/mobile-survey.sh
+++ docker/images/mobile-survey/mobile-survey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export METEOR_SETTINGS=$(cat /shared/settings-production.json)
-meteor node /mobile-survey-webapp/app/build/bundle/main.js 
+node /mobile-survey-webapp/app/build/bundle/main.js 
 
