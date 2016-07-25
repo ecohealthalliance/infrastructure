4394cfb1b364b931b0e19c7e4815df008a4f2d61
Nathan Breit <nath@nathanathan.com>
2016-07-25 12:45:14 -0400

    add other ansible params  + use ssh for git clone

--- docker/images/promed-scraper/cronjob.sh
+++ docker/images/promed-scraper/cronjob.sh
@@ -3,5 +3,4 @@ env
 source /docker.env
 echo "------------------------------"
 env
-# TODO: setup aws credentials in dockerfile?
-/usr/local/bin/ansible-playbook --connection=local /ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL"
+/usr/local/bin/ansible-playbook --connection=local /ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" $OTHER_ANSIBLE_PARAMS
