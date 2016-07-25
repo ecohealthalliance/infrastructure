66a2d5c8e174a658f8f3ac53fd3d9969d9bf64e6
Nathan Breit <nath@nathanathan.com>
2016-07-25 14:08:02 -0400

    Fix playbook path

--- docker/images/promed-scraper/cronjob.sh
+++ docker/images/promed-scraper/cronjob.sh
@@ -1,6 +1,4 @@
 echo "Print out variables for testing"
 env
 source /docker.env
-echo "------------------------------"
-env
-/usr/local/bin/ansible-playbook --connection=local /ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" $OTHER_ANSIBLE_PARAMS
+/usr/local/bin/ansible-playbook --connection=local /promed_mail_scraper/ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" $OTHER_ANSIBLE_PARAMS
