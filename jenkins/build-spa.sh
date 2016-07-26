a496cc7a89197e943c7723c3a2d7bb8dcf601235
Nathan Breit <nath@nathanathan.com>
2016-07-26 10:23:17 -0400

    use promed scraper master branch

--- jenkins/build-spa.sh
+++ jenkins/build-spa.sh
@@ -4,6 +4,6 @@
 git checkout master
 git submodule foreach git pull origin master
 rm -rf docker/images/promed-scraper/promed_mail_scraper
-git clone -b bsve git@github.com:ecohealthalliance/promed_mail_scraper.git docker/images/promed-scraper/promed_mail_scraper
+git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git docker/images/promed-scraper/promed_mail_scraper
 docker build -t spa docker/images/spa
 docker build -t promed-scraper docker/images/promed-scraper
