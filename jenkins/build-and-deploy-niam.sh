f3c2e6dd8eab45d4d9f16ff5644ab481b3eb5fe3
Freddie Rosario <rosario@ecohealthalliance.org>
2016-08-17 11:14:46 -0400

    Adding tag to only change niam to keep deployment times down

--- jenkins/build-and-deploy-niam.sh
+++ jenkins/build-and-deploy-niam.sh
@@ -2,4 +2,5 @@
 
 cd /opt/infrastructure/ansible/niam/ &&\
 ansible-galaxy install -r requirements.yml &&\
-ansible-playbook site.yml --become-user=ubuntu --vault-password-file /keys/grits_vault_password --private-key /keys/infrastructure.pem 
+ansible-playbook site.yml --become-user=ubuntu --vault-password-file /keys/grits_vault_password --private-key /keys/infrastructure.pem --tags niam
+
