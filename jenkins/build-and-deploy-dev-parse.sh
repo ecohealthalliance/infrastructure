7de04551ccfe282f12ae56448ab5dc5ec2407858
Freddie Rosario <rosario@ecohealthalliance.org>
2016-08-19 11:57:53 -0400

    Adding build-and-deploy-dev-parse.sh

---
+++ jenkins/build-and-deploy-dev-parse.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Note: This script is intended for jenkins.
+
+export remote_command="ssh -ti /keys/infrastructure.pem ubuntu@dev-survey.eha.io"
+
+#Make sure repo is up to date
+$remote_command "cd /opt/infrastructure && git pull"
+
+#Build the new image
+$remote_command "sudo docker build -t parse-server /opt/infrastructure/docker/images/parse-server"
+
+#Instantiate the new image
+$remote_command "sudo docker-compose -f /opt/infrastructure/docker/containers/parse-server.yml up -d"
+
+
