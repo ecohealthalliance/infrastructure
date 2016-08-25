f04d180b805b9b1ca806ba1c446234a7553184f8
Freddie Rosario <rosario@ecohealthalliance.org>
2016-08-25 15:27:20 -0400

    First pass at automating era scripts

---
+++ jenkins/era-scripts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+(cd ERA || git clone https://github.com/ecohealthalliance/ERA.git)
+git pull master
+(docker run --name mongo-client -d mongodb sleep infinity || true)
+docker cp .scripts mongo-client:/scripts
+
+docker exec -t mongo-client mongo mongodb://flirt-reporting.eha.io/flirt /scripts/flight_counts.js
+
+docker exec -t mongo-client mongo mongodb://flirt-reporting.eha.io/flirt /scripts/day_counts.js
+
+docker exec -t mongo-client mongo mongodb://flirt-reporting.eha.io/flirt /scripts/airport_counts.js
+
