2b5915d2e3416772ecd63bcc05297d7a0c4c939c
Freddie Rosario <rosario@ecohealthalliance.org>
2016-09-07 09:34:48 -0400

    Building eidr-connect by disabling cache

--- jenkins/build-and-deploy-eidr-connect.sh
+++ jenkins/build-and-deploy-eidr-connect.sh
@@ -7,7 +7,7 @@ export remote_command="ssh -ti /keys/infrastructure.pem ubuntu@eidr-connect.eha.
 $remote_command "cd /opt/eidr-connect && git pull"
 
 #Build the new image
-$remote_command "sudo docker build -t eidr-connect /opt/eidr-connect"
+$remote_command "sudo docker build --no-cache -t eidr-connect /opt/eidr-connect"
 
 #Instantiate the new image
 $remote_command "sudo docker-compose -f /opt/eidr-connect/eidr-connect.yml up -d"
