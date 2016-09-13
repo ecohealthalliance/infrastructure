f3f57e39f2a777efd7c80de2c9b7e0df6229a67c
Abe Miessler <miessler@ecohealthalliance.org>
2016-09-13 08:58:23 -0700

    flirt update script

--- jenkins/flirt-update.sh
+++ jenkins/flirt-update.sh
@@ -1,44 +1,45 @@
-# ssh into flirt machine
-# go into docker instance for web
 
-# cd /grits
+echo "*****get initial flight/legs counts*****"
+/usr/bin/mongo 192.168.1.118/grits-net-meteor --eval 'db.flights.count()'
+/usr/bin/mongo 192.168.1.118/grits-net-meteor --eval 'db.legs.count()'
 
-# open run.shell
-
-# Execute all lines for the following blocks:
-
-echo "get initial flight/legs counts"
 
 # This is the default run.sh..
 # Should only be executed on first run.
 # If you have a prepopulated DB, just execute the container with the last line
 
-echo "Take care of grits-net-consume dependencies"
-# cd /grits-net-consume
-# virtualenv /grits-net-consume-env &&\
-# source /grits-net-consume-env/bin/activate &&\
-# pip install -r requirements.txt
+echo "*****Take care of grits-net-consume dependencies*****"
+# cd /Users/Shared/Jenkins/grits-net-consume
 
-echo "Update ftp credentials"
-# sed -i 's/url-innovata.com/suwweb03.innovata-llc.com/' grits_ftp_config.py
-# sed -i 's/username/ECOHEALTH/' grits_ftp_config.py
-# sed -i 's/password/HD38jFXc/' grits_ftp_config.py
+# echo "change directories"
+cd /grits-net-consume
+# echo "virtualenv"
+virtualenv /grits-net-consume-env
+# echo "source"
+source /grits-net-consume-env/bin/activate
+# echo "install"
+pip install -r requirements.txt
 
-echo "Consume/import flight data"
-# python grits_flight_pull.py
-# python grits_consume.py --type DiioAirport -m 10.0.0.175 -d grits-net-meteor tests/data/MiExpressAllAirportCodes.tsv
-# python grits_consume.py --type FlightGlobal -m 10.0.0.134 -d grits-net-meteor data/EcoHealth_*.csv  &&\
-# rm -fr data
+echo "*****Update ftp credentials*****"
+sed -i 's/url-innovata.com/suwweb03.innovata-llc.com/' grits_ftp_config.py
+sed -i 's/username/ECOHEALTH/' grits_ftp_config.py
+sed -i 's/password/HD38jFXc/' grits_ftp_config.py
 
-echo "Make sure indexes are good"
-# python grits_ensure_index.py -d grits-net-meteor -m 10.0.0.134
+echo "*****Consume/import flight data*****"
+python grits_flight_pull.py
+python grits_consume.py --type DiioAirport -m 192.168.1.118 -d grits-net-meteor tests/data/MiExpressAllAirportCodes.tsv
+python grits_consume.py --type FlightGlobal -m 192.168.1.118 -d grits-net-meteor data/EcoHealth_*.csv  &&\
+rm -fr data
 
-echo "Import heat map data"
-# aws s3 sync s3://flight-network-heat-map/ ./
-# mongorestore -h 10.0.0.134 -d grits-net-meteor -c heatmap ./dump/grits/heatmap.bson
+echo "*****Make sure indexes are good*****"
+python grits_ensure_index.py -d grits-net-meteor -m 192.168.1.118
 
-echo "Create legs collection"
-# java -jar /flirt-legs.jar --mongohost="10.0.0.134" mongoport="27017"
+echo "*****Import heat map data*****"
+aws s3 sync s3://flight-network-heat-map/ ./
+mongorestore -h 192.168.1.118 -d grits-net-meteor -c heatmap ./dump/grits/heatmap.bson
 
-echo "Start the app"
-# service supervisor start
+echo "*****Create legs collection*****"
+java -jar /flirt-legs.j                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 ar --mongohost="192.168.1.118" mongoport="27017"
+
+echo "*****Start the app*****"
+service supervisor start
