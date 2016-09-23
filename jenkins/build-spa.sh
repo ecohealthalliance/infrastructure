5254e5f2b27f09544785ade4c124583b318aba27
Freddie Rosario <rosario@ecohealthalliance.org>
2016-09-23 09:17:33 -0400

    Adding logical ands to build-spa.sh to get it to fail on first error

--- jenkins/build-spa.sh
+++ jenkins/build-spa.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #This is a jenkins script
 
-git checkout master
-git submodule foreach git pull origin master
-rm -rf docker/images/promed-scraper/promed_mail_scraper
-git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git docker/images/promed-scraper/promed_mail_scraper
+git checkout master &&\
+git submodule foreach git pull origin master &&\
+rm -rf docker/images/promed-scraper/promed_mail_scraper &&\
+git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git docker/images/promed-scraper/promed_mail_scraper &&\
 docker build -t spa docker/images/spa
-# Start netcat process to serve vault password to build script to docker build process.
+
+# Start netcat process to serve vault password to build script to docker build process. 
 # This is done so that the password can be used in a single command
 # without it being stored in the image's history/metadata.
 nc -l 14242 < /vault-passwords/grits &
