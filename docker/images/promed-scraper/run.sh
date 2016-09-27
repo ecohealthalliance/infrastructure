acb3dc736f928a127de79e6474a89b0df99ea674
Nathan Breit <nath@nathanathan.com>
2016-09-27 07:34:05 -0400

    run promed scraper cronjob at random hour

--- docker/images/promed-scraper/run.sh
+++ docker/images/promed-scraper/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Create cronjob to run ProMED mail ingest.
+# A CRON_SPECIFIER environment variable can be provided.
+# Otherwise the script will run at a randomly chosen hour each day.
+RANDOM_HOUR=$(($RANDOM % 24))
+DEFAULT_CRON_SPECIFIER="0 $RANDOM_HOUR * * *"
+CRON_SPECIFIER="${CRON_SPECIFIER:-$DEFAULT_CRON_SPECIFIER}"
+echo "$CRON_SPECIFIER bash /cronjob.sh >> /cron.log 2>&1" > /etc/cron.d/crontab
+crontab /etc/cron.d/crontab
 env > /docker.env
 # link to aws config
 ln -s /shared/.aws /root
