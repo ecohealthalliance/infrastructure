0a778bed6ceb8d215b177b07d46baa8042540fdf
Abe Miessler <miessler@ecohealthalliance.org>
2016-10-14 09:57:24 -0700

    pulling out scraper stuff from spa deploy

--- jenkins/deploy-promed-scraper.sh
+++ jenkins/deploy-promed-scraper.sh
@@ -9,7 +9,6 @@
 /bin/echo "Exported images now compressed"
 
 #Store the image in an s3 bucket for the BSVE deployment scripts
-# aws s3 cp /tmp/spa.tar.gz s3://bsve-integration/spa.tar.gz &&\
 aws s3 cp /tmp/promed-scraper.tar.gz s3://bsve-integration/promed-scraper.tar.gz &&\
 /bin/echo "Images uploaded"
 
@@ -18,18 +17,13 @@ export ssh_command="/usr/bin/ssh -i /keys/infrastructure.pem  ubuntu@spa.eha.io 
 function scp_file { /usr/bin/scp -i /keys/infrastructure.pem $1 ubuntu@spa.eha.io:/tmp/; }
 
 #Import images on demo box
-# scp_file /tmp/spa.tar.gz &&\
 scp_file /tmp/promed-scraper.tar.gz &&\
-# $ssh_command /bin/gzip -d /tmp/spa.tar.gz &&\
 $ssh_command /bin/gzip -d /tmp/promed-scraper.tar.gz &&\
-# $ssh_command "/usr/bin/sudo /usr/bin/docker load < /tmp/spa.tar" &&\
 $ssh_command "/usr/bin/sudo /usr/bin/docker load < /tmp/promed-scraper.tar" &&\
 /bin/echo "Images now imported on remote"
 
 #Clean up old data
-# /bin/rm /tmp/spa.tar.gz &&\
 /bin/rm /tmp/promed-scraper.tar.gz &&\
-# $ssh_command /bin/rm /tmp/spa.tar &&\
 $ssh_command /bin/rm /tmp/promed-scraper.tar
 
 #Setup AWS credentials in shared directory
@@ -37,6 +31,6 @@ scp_file /var/lib/jenkins/.aws/credentials &&\
 $ssh_command "sudo /bin/mkdir -p /shared/.aws" &&\
 $ssh_command "sudo /bin/mv /tmp/credentials /shared/.aws"
 
-#Reprovision containers
-# scp_file docker/containers/promed-scraper.yml &&\
-# $ssh_command "sudo docker-compose -f /tmp/promed-scraper.yml up -d"
+# Reprovision containers
+scp_file docker/containers/promed-scraper.yml &&\
+$ssh_command "sudo docker-compose -f /tmp/promed-scraper.yml up -d"
