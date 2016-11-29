0d532a275b60a3c1e82bbd7d591bd812d471b2ff
Nathan Breit <nabreit@gmail.com>
2016-11-30 00:04:42 +0800

    Pull repo to debug skipped dockerfile step

--- jenkins/build-spa.sh
+++ jenkins/build-spa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This is a jenkins script
 
-git checkout master &&\
+git checkout master && git pull &&\
 git submodule foreach git pull origin master &&\
 # rm -rf docker/images/promed-scraper/promed_mail_scraper &&\
 # git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git docker/images/promed-scraper/promed_mail_scraper &&\
