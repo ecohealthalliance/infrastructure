a4f53c837519d9a58aa24f4d78f134bcf3022f8c
Freddie Rosario <rosario@ecohealthalliance.org>
2016-11-29 16:19:41 -0500

    Update

--- jenkins/docker-clean-orphans.sh
+++ jenkins/docker-clean-orphans.sh
@@ -21,6 +21,16 @@ docker_servers=(
   spa.eha.io
                )
 
+#SSH to each machine, and clean up
+for server in "${docker_servers[@]}"; do
+  echo "Cleaning $server..."
+  cleanup $server;
+done
+
+#NIAM access is a little different
+echo "Cleaing Niam..."
+ssh ubuntu@niam.eha.io $sudo_remove_orphans
+
 #Cleanup jenkins itself
 echo "Cleaning Jenkins..."
 docker ps -a |egrep "Exited|Created"
@@ -36,14 +46,3 @@ if [[ $? -eq 0 ]];then
   $remove_orphans
 fi
 
-#SSH to each machine, and clean up
-for server in "${docker_servers[@]}"; do
-  echo "Cleaning $server..."
-  cleanup $server;
-done
-
-#NIAM access is a little different
-echo "Cleaing Niam..."
-ssh ubuntu@niam.eha.io $sudo_remove_orphans
-
-echo "All done!"
