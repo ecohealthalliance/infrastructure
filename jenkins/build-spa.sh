3a57c63c37a96a19934d6b379e17e3a100708a20
Nathan Breit <nabreit@gmail.com>
2016-11-29 23:05:22 +0800

    disable docker cache

--- jenkins/build-spa.sh
+++ jenkins/build-spa.sh
@@ -5,7 +5,7 @@ git checkout master &&\
 git submodule foreach git pull origin master &&\
 # rm -rf docker/images/promed-scraper/promed_mail_scraper &&\
 # git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git docker/images/promed-scraper/promed_mail_scraper &&\
-docker build -t spa docker/images/spa
+docker --no-cache build -t spa docker/images/spa
 
 # Start netcat process to serve vault password to build script to docker build process. 
 # This is done so that the password can be used in a single command
