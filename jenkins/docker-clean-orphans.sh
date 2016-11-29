dc1552d1a0720ef85d769058c0138f06daf9668a
Freddie Rosario <rosario@ecohealthalliance.org>
2016-11-29 16:13:11 -0500

    Update to docker-clean-orphans.sh

--- jenkins/docker-clean-orphans.sh
+++ jenkins/docker-clean-orphans.sh
@@ -1,10 +1,13 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 # Purpose: Remove docker images labeled as "none"
 # Author: Freddie Rosario <rosario@ecohealthalliance.org>
 
+remove_stopped_containers="docker ps -a|egrep \"Exited|Created\"|awk '{print $1}'|xargs docker rm -f"
+remove_orphans="docker images|grep none|awk '{print $3}'|xargs docker rmi"
+sudo_remove_orphans="sudo docker images|grep none|awk '{print $3}'|xargs sudo docker rmi"
 
 cleanup(){
-  ssh -ti /keys/infrastructure.pem ubuntu@$1 "sudo docker images|grep none|awk '{print $3}'|xargs sudo docker rmi";
+  ssh -i /keys/infrastructure.pem ubuntu@$1 $sudo_remove_orphans
 }
 
 docker_servers=(
@@ -24,22 +27,23 @@ docker ps -a |egrep "Exited|Created"
 
 #Check for and remove stopped containers
 if [[ $? -eq 0 ]];then
-  docker ps -a|egrep "Exited|Created"|awk '{print $1}'|xargs docker rm -f
+  $remove_stopped_containers
 fi
 
 #Check for none images
 docker images|grep none
 if [[ $? -eq 0 ]];then
-  docker images|grep none|awk '{print $3}'|xargs docker rmi
+  $remove_orphans
 fi
 
 #SSH to each machine, and clean up
-for server in $docker_servers;do
+for server in "${docker_servers[@]}"; do
   echo "Cleaning $server..."
   cleanup $server;
 done
 
 #NIAM access is a little different
 echo "Cleaing Niam..."
-ssh -t ubuntu@niam.eha.io $remove_orphans
+ssh ubuntu@niam.eha.io $sudo_remove_orphans
 
+echo "All done!"
