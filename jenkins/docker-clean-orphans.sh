c523f4b1d18f7292cb344ad80df9a6b90c8b5d76
Freddie Rosario <rosario@ecohealthalliance.org>
2016-11-29 11:59:19 -0500

    Updated job that cleans docker orphans

--- jenkins/docker-clean-orphans.sh
+++ jenkins/docker-clean-orphans.sh
@@ -3,5 +3,31 @@
 # Purpose: Remove docker images labeled as "none"
 # Author: Freddie Rosario <rosario@ecohealthalliance.org>
 
-docker images|grep none|awk '{print $3}'|xargs docker rmi
+remove_orphans="docker images|grep none|awk '{print $3}'|xargs docker rmi"
+
+cleanup(){
+  ssh -ti /keys/infrastructure.pem ubuntu@$1 $remove_orphans;
+}
+
+docker_servers=(
+  dev-parse.eha.io
+  parse.eha.io
+  eidr-connect.eha.io
+  grits.eha.io
+  grits-dev.eha.io
+  birt.eha.io
+  era.eha.io
+  spa.eha.io
+               )
+
+#Remove local orphans on jenkins itself
+$remove_orphans
+
+#SSH to each machine, and clean up
+for server in $docker_servers;do
+  cleanup $server;
+done
+
+#NIAM access is a little different
+ssh -ti ubuntu@niam.eha.io $remove_orphans
 
