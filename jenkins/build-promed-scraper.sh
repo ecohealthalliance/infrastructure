e7fe7d4ace478bffb0c0308535588792b622fa6f
Abe Miessler <miessler@ecohealthalliance.org>
2016-12-08 09:15:27 -0800

    moving netcat logic

--- jenkins/build-promed-scraper.sh
+++ jenkins/build-promed-scraper.sh
@@ -3,5 +3,12 @@
 
 git checkout master && git pull &&\
 git submodule foreach git pull origin master &&\
+rm -rf docker/images/promed-scraper/promed_mail_scraper &&\
 git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git /opt/infrastructure/docker/images/promed-scraper/promed_mail_scraper 
+# Start netcat process to serve vault password to build script to docker build process. 
+# This is done so that the password can be used in a single command
+# without it being stored in the image's history/metadata.
+nc -l 14242 < /vault-passwords/grits &
 docker build --no-cache -t promed-scraper docker/images/promed-scraper
+# Ensure the netcat process terminates
+nc localhost 14242
--- jenkins/build-spa.sh
+++ jenkins/build-spa.sh
@@ -10,7 +10,7 @@ docker build --no-cache -t spa docker/images/spa
 # Start netcat process to serve vault password to build script to docker build process. 
 # This is done so that the password can be used in a single command
 # without it being stored in the image's history/metadata.
-nc -l 14242 < /vault-passwords/grits &
+# nc -l 14242 < /vault-passwords/grits &
 # docker build -t promed-scraper docker/images/promed-scraper
 # Ensure the netcat process terminates
-nc localhost 14242
+# nc localhost 14242
