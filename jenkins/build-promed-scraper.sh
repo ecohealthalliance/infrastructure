969b83c2a5133444ad0dc19d5807c13f2c985bad
Abe Miessler <miessler@ecohealthalliance.org>
2016-12-15 14:51:14 -0800

    backing up image to s3

--- jenkins/build-promed-scraper.sh
+++ jenkins/build-promed-scraper.sh
@@ -8,7 +8,16 @@ git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git /op
 # Start netcat process to serve vault password to build script to docker build process. 
 # This is done so that the password can be used in a single command
 # without it being stored in the image's history/metadata.
+
 nc -l 14242 < /vault-passwords/grits &
 docker build --no-cache -t promed-scraper docker/images/promed-scraper
+
+docker save promed-scraper > /tmp/promed-scraper.tar &&\
+echo "Docker image exported" &&\
+zip -1 /tmp/promed-scraper.tar &&\
+echo "Exported image now compressed" &&\
+aws s3 cp /tmp/promed-scraper.tar.gz s3://bsve-integration/promed-scraper.tar.gz &&\
+echo "Compressed image backed up to s3://bsve-integration/promed-scraper.tar.gz"
+
 # Ensure the netcat process terminates
 nc -v localhost 14242 || echo 'Ensure the netcat process terminates'
