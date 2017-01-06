9e22aa65233fcb85adaa755c260cc706046db383
Abe Miessler <miessler@ecohealthalliance.org>
2017-01-06 07:48:22 -0800

    adding revisions.txt logic back in

--- jenkins/build-and-deploy-eidr-connect.sh
+++ jenkins/build-and-deploy-eidr-connect.sh
@@ -4,7 +4,7 @@
 export remote_command="ssh -ti /keys/infrastructure.pem ubuntu@eidr-connect.eha.io"
 
 #Make sure repo is up to date
-$remote_command "cd /opt/eidr-connect && git checkout release; git pull"
+$remote_command "cd /opt/eidr-connect && git checkout release; git pull; git rev-parse HEAD > revision.txt"
 
 #Build the new image
 $remote_command "sudo docker build --no-cache -t eidr-connect /opt/eidr-connect"
