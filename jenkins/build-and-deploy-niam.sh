fc8c3f16a3d4d84d3927ad52d98085d454f0d4af
Freddie Rosario <rosario@ecohealthalliance.org>
2017-01-12 14:58:32 -0500

    Upload latest niam to s3

--- jenkins/build-and-deploy-niam.sh
+++ jenkins/build-and-deploy-niam.sh
@@ -3,5 +3,12 @@
 cd /opt/infrastructure/ansible/niam/ &&\
 ansible-galaxy install -r requirements.yml &&\
 ansible-playbook site.yml --become-user=ubuntu --vault-password-file /keys/grits_vault_password --private-key /var/lib/jenkins/.ssh/id_rsa --tags niam
+
+#Upload new docker image to S3
+docker save niam > /tmp/niam.tar &&\
+gzip -1 /tmp/niam.tar &&\
+aws s3 cp /tmp/niam.tar.gz s3://bsve-integration/niam.tar.gz
+rm /tmp/niam.tar.gz
+
 # Notify BSVE to redeploy
 aws sns publish --topic-arn arn:aws:sns:us-east-1:789867670404:EHA-Git-Lambda --message '{"app":"niam"}' --profile bsve-user
