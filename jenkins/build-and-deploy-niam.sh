5a9aa279b2c5a74bc6246cf82f3397184cd539c2
Nathan Breit <nabreit@gmail.com>
2017-01-12 10:14:11 -0500

    Add NIAM SNS message

--- jenkins/build-and-deploy-niam.sh
+++ jenkins/build-and-deploy-niam.sh
@@ -3,4 +3,5 @@
 cd /opt/infrastructure/ansible/niam/ &&\
 ansible-galaxy install -r requirements.yml &&\
 ansible-playbook site.yml --become-user=ubuntu --vault-password-file /keys/grits_vault_password --private-key /var/lib/jenkins/.ssh/id_rsa --tags niam
-
+# Notify BSVE to redeploy
+aws sns publish --topic-arn arn:aws:sns:us-east-1:789867670404:EHA-Git-Lambda --message '{"app":"niam"}' --profile bsve-user
