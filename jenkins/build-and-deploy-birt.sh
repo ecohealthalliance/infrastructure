f82654c66edd8599969796cc76ff61d91441203b
Freddie Rosario <rosario@ecohealthalliance.org>
2017-01-13 15:19:51 -0500

    Deploy birt to s3

--- jenkins/build-and-deploy-birt.sh
+++ jenkins/build-and-deploy-birt.sh
@@ -11,3 +11,11 @@ $remote_command "sudo docker build --no-cache -t birt /opt/infrastructure/docker
 #Instantiate the new image
 $remote_command "sudo docker-compose -f /opt/infrastructure/docker/containers/birt.yml up -d birt"
 
+#Upload new docker image to S3
+$remote_command "
+  sudo rm /tmp/birt.tar.gz
+  sudo docker save birt > /tmp/birt.tar &&\
+  sudo gzip -1 /tmp/birt.tar &&\
+  sudo aws s3 cp /tmp/birt.tar.gz s3://bsve-integration/birt.tar.gz
+  sudo rm /tmp/birt.tar.gz
+"
