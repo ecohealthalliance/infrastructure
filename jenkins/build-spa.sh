ce1d2af766e9b3d09b280c7b59f7eb3d0b510cda
Freddie Rosario <rosario@ecohealthalliance.org>
2017-01-19 14:45:55 -0500

    Refactor spa docker image

--- jenkins/build-spa.sh
+++ jenkins/build-spa.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #This is a jenkins script
 
-git checkout master && git pull &&\
-git submodule foreach git pull origin master &&\
-# rm -rf docker/images/promed-scraper/promed_mail_scraper &&\
-# git clone -b master git@github.com:ecohealthalliance/promed_mail_scraper.git docker/images/promed-scraper/promed_mail_scraper &&\
 docker build --no-cache -t spa docker/images/spa
 
 # Start netcat process to serve vault password to build script to docker build process. 
