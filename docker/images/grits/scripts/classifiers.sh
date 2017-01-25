35f81933ea83d88c7554d2d37c741746c5f17569
Freddie Rosario <rosario@ecohealthalliance.org>
2017-01-25 12:09:11 -0500

    Grits image: Bundle classifier data

--- docker/images/grits/scripts/classifiers.sh
+++ docker/images/grits/scripts/classifiers.sh
@@ -3,12 +3,12 @@
 source /source-vars.sh
 
 #Download classifiers
-mkdir $GRITS_HOME/classifiers/
-aws --no-sign-request s3 sync s3://classifier-data/classifiers/ $GRITS_HOME/classifiers/ || exit 1
+#mkdir $GRITS_HOME/classifiers/
+#aws --no-sign-request s3 sync s3://classifier-data/classifiers/ $GRITS_HOME/classifiers/ || exit 1
 
 #Find the newest then link it
-export CLASSIFIER_PATH=$(ls -t $GRITS_HOME/classifiers/*/*.p | head -1 | xargs dirname)
-ln -s $CLASSIFIER_PATH $GRITS_HOME/grits-api/current_classifier
+#export CLASSIFIER_PATH=$(ls -t $GRITS_HOME/classifiers/*/*.p | head -1 | xargs dirname)
+#ln -s $CLASSIFIER_PATH $GRITS_HOME/grits-api/current_classifier
 
 #Import keywords into mongo for annotation
 cd $GRITS_HOME/grits-api/
