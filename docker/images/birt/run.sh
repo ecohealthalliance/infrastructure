945c72e50018aa155883940cac768bcd8455eff1
Freddie Rosario <rosario@ecohealthalliance.org>
2017-02-03 09:24:13 -0500

    Update to run.sh

--- docker/images/birt/run.sh
+++ docker/images/birt/run.sh
@@ -6,7 +6,7 @@ mongorestore -h 172.30.2.160 -d promed -c blindspots ./dump/promed/blindspots.bs
 
 #Run the annual sightings script
 cd /birt-meteor/tools &&\
-npm run-script annualSightings
+npm run-script annualSighting -u $MONGO_URL
 
 #Start the app
 service supervisor start
