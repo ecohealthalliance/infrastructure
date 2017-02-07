30ce633b5575aea6343189c19eb2776beff32443
Nathan Breit <nabreit@gmail.com>
2017-02-07 19:52:00 +0800

    Fix annual sighting command

--- docker/images/birt/run.sh
+++ docker/images/birt/run.sh
@@ -6,7 +6,7 @@ mongorestore -h 172.30.2.160 -d promed -c blindspots ./dump/promed/blindspots.bs
 
 #Run the annual sightings script
 cd /birt-meteor/tools &&\
-npm run-script annualSighting -u $MONGO_URL
+node build/annualSightings.js -u $MONGO_URL
 
 #Start the app
 service supervisor start
