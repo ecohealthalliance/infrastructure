a3caa31d996ab9a851dfd8ecbe813e555617af5e
Nathan Breit <nath@nathanathan.com>
2017-02-13 13:31:02 -0500

    download prebuilt db and fix geoname lookup bug

--- docker/images/grits/run.sh
+++ docker/images/grits/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-$GRITS_HOME/grits_env/bin/python -m annotator.sqlite_import_geonames
-supervisord --nodaemon --config /etc/supervisor/supervisord.conf
+# Try to download premade database
+wget -O $ANNOTATOR_DB_PATH https://s3.amazonaws.com/bsve-integration/annotator.sqlitedb
+$GRITS_HOME/grits_env/bin/python -m annotator.sqlite_import_geonames && supervisord --nodaemon --config /etc/supervisor/supervisord.conf
