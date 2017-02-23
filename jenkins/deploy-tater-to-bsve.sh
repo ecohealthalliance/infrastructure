8b0aa96eb0d9334e5c26de9197adbc670ffbcbd1
Abe Miessler <miessler@ecohealthalliance.org>
2017-02-23 10:27:51 -0800

    adding region to sns call

--- jenkins/deploy-tater-to-bsve.sh
+++ jenkins/deploy-tater-to-bsve.sh
@@ -7,6 +7,6 @@ gzip -1 /tmp/tater.tar
 
 aws s3 cp /tmp/tater.tar.gz s3://bsve-integration/tater.tar.gz
 
-aws sns publish --topic-arn arn:aws:sns:us-east-1:789867670404:EHA-Git-Lambda --message '{"app":"tater"}' --profile bsve-user
+aws sns publish --topic-arn arn:aws:sns:us-east-1:789867670404:EHA-Git-Lambda --message '{"app":"tater"}' --profile bsve-user --region us-east-1
 
 
