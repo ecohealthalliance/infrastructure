05c0bd6b774fac89e072ca9ad463ecb366838fa6
Freddie Rosario <rosario@ecohealthalliance.org>
2017-02-23 12:30:58 -0500

    New script to deploy tater to bsve

---
+++ jenkins/deploy-tater-to-bsve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -fr /tmp/tater.tar*
+
+docker save tater > /tmp/tater.tar
+gzip -1 /tmp/tater.tar
+
+aws s3 cp /tmp/tater.tar.gz s3://bsve-integration/tater.tar.gz
+
+sns publish --topic-arn arn:aws:sns:us-east-1:789867670404:EHA-Git-Lambda --message '{"app":"tater"}' --profile bsve-user
+
+
