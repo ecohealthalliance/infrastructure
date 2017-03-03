ae6672f68f45f3a58ca6158e129318868ed25223
Abe Miessler <miessler@ecohealthalliance.org>
2017-03-03 11:44:49 -0800

    moving docker build logic to the ERA machine instead of building on the Jenkins machine

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #This is a jenkins script
+export remote_command="/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@era.eha.io "
 
-cd /opt/infrastructure/docker/images/era/era
-git pull origin master
-cd /opt/infrastructure
-docker build -t era /opt/infrastructure/docker/images/era
+$remote_command "cd /opt/infrastructure/docker/images/era/era && git pull origin master"
+$remote_command "docker build -t era /opt/infrastructure/docker/images/era"
 
