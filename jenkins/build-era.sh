baa4850029f2cf4288992fb3a91174f4bf15c615
Abe Miessler <miessler@ecohealthalliance.org>
2017-03-03 12:05:01 -0800

    pulling out key

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is a jenkins script
-export remote_command="/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@era.eha.io "
+export remote_command="/usr/bin/ssh ubuntu@era.eha.io "
 
 $remote_command "cd /opt/infrastructure/docker/images/era/era && git pull origin master"
 $remote_command "docker build -t era /opt/infrastructure/docker/images/era"
