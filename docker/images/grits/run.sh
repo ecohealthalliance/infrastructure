d0b72ba5975211e76cbf9a56bfdb33deac14963b
Nathan Breit <nabreit@gmail.com>
2017-03-15 17:06:37 +0800

    Call disease ontology import script

--- docker/images/grits/run.sh
+++ docker/images/grits/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Try to download premade database
 wget -O $ANNOTATOR_DB_PATH https://s3.amazonaws.com/bsve-integration/annotator.sqlitedb
-$GRITS_HOME/grits_env/bin/python -m annotator.sqlite_import_geonames && supervisord --nodaemon --config /etc/supervisor/supervisord.conf
+(
+  $GRITS_HOME/grits_env/bin/python -m annotator.sqlite_import_disease_ontology &&
+  $GRITS_HOME/grits_env/bin/python -m annotator.sqlite_import_geonames &&
+  supervisord --nodaemon --config /etc/supervisor/supervisord.conf
+)
