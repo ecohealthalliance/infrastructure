db127cc79c59dfba8d4983f631740df72220612e
Nathan Breit <nath@nathanathan.com>
2017-03-15 06:00:59 -0400

    Remove no-longer-required geoname import script

--- docker/images/grits/scripts/first-run.sh
+++ docker/images/grits/scripts/first-run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 /scripts/update-settings.sh &&\
-/scripts/geonames.sh &&\
 /scripts/disease-label-autocomplete.sh &&\
 /scripts/classifiers.sh
 
--- docker/images/grits/scripts/geonames.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $GRITS_HOME/annie &&\
-curl -O http://download.geonames.org/export/dump/allCountries.zip &&\
-unzip allCountries.zip &&\
-$GRITS_HOME/grits_env/bin/python mongo_import_geonames.py --mongo_url $MONGO_URL
-
-
