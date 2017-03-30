53cf9a3333f09596715ee2255fbdb6233c21250f
Freddie Rosario <rosario@ecohealthalliance.org>
2017-03-29 21:16:23 -0400

    Update to shell script that launches dashboard

--- docker/images/grits/diagnostic-dashboard/dashboard.sh
+++ docker/images/grits/diagnostic-dashboard/dashboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /source-vars.sh  &&\
 cd $GRITS_HOME/diagnostic-dashboard &&\
-node $GRITS_HOME/diagnostic-dashboard/bundle/main.js
+meteor node $GRITS_HOME/diagnostic-dashboard/bundle/main.js
 
