53343a0e03067542486db201544167cede8a710a
Nathan Breit <nath@nathanathan.com>
2017-04-20 11:49:16 -0400

    fix celery zombie process bug

--- docker/images/grits/grits-api/celery_diagnose.sh
+++ docker/images/grits/grits-api/celery_diagnose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /source-vars.sh  &&\
 cd $GRITS_HOME/grits-api &&\
-$GRITS_HOME/grits_env/bin/celery worker -A tasks -Q diagnose,priority --loglevel=INFO --concurrency=$DIAGNOSE_WORKERS
+exec $GRITS_HOME/grits_env/bin/celery worker -A tasks -Q diagnose,priority --loglevel=INFO --concurrency=$DIAGNOSE_WORKERS
--- docker/images/grits/grits-api/celery_priority.sh
+++ docker/images/grits/grits-api/celery_priority.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /source-vars.sh  &&\
 cd $GRITS_HOME/grits-api &&\
-$GRITS_HOME/grits_env/bin/celery worker -A tasks -Q priority --loglevel=INFO --concurrency=$PRIORITY_WORKERS
+exec $GRITS_HOME/grits_env/bin/celery worker -A tasks -Q priority --loglevel=INFO --concurrency=$PRIORITY_WORKERS
