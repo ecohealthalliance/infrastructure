932fea53975c5735afd52c1a337c9af08e6dcff1
Freddie Rosario <rosario@ecohealthalliance.org>
2017-05-15 13:20:02 -0400

    Error logic to build-and-deploy-eidr-connect.sh

--- jenkins/build-and-deploy-eidr-connect.sh
+++ jenkins/build-and-deploy-eidr-connect.sh
@@ -4,13 +4,13 @@
 export remote_command="ssh -ti /var/lib/jenkins/.ssh/id_rsa ubuntu@eidr-connect.eha.io"
 
 #Make sure repo is up to date
-$remote_command "cd /opt/eidr-connect && git checkout release; git pull; git rev-parse HEAD > revision.txt"
+$remote_command "cd /opt/eidr-connect && git checkout release; git pull; git rev-parse HEAD > revision.txt" &&\
 
 #Build the new image
-$remote_command "sudo docker build --no-cache -t eidr-connect /opt/eidr-connect"
+$remote_command "sudo docker build --no-cache -t eidr-connect /opt/eidr-connect" &&\
 
 #Forcefully remove previous running container
-$remote_command "sudo docker rm -f eidr-connect.eha.io || true"
+$remote_command "sudo docker rm -f eidr-connect.eha.io || true" &&\
 
 #Instantiate the new image
 $remote_command "sudo docker-compose -f /opt/eidr-connect/eidr-connect.yml up -d"
