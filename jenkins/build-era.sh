2f5bb65fe89fad0ddf649a0c5ddf2dd510b8db8c
Freddie Rosario <rosario@ecohealthalliance.org>
2017-05-15 13:39:12 -0400

    Error logic for build-era.sh

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -2,6 +2,6 @@
 #This is a jenkins script
 export remote_command="/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@era.eha.io "
 
-$remote_command "cd /opt/infrastructure/docker/images/era/era && /usr/bin/sudo git pull origin master"
+$remote_command "cd /opt/infrastructure/docker/images/era/era && /usr/bin/sudo git pull origin master" &&\
 $remote_command "/usr/bin/sudo docker build -t era /opt/infrastructure/docker/images/era"
 
