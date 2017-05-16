9222d06baaf571e1119f1c41fadbe352c03f6bc1
Freddie Rosario <rosario@ecohealthalliance.org>
2017-05-16 09:57:32 -0400

    Error logic for deploy-kirby-celery-worker.sh

--- jenkins/deploy-kirby-celery-worker.sh
+++ jenkins/deploy-kirby-celery-worker.sh
@@ -6,13 +6,13 @@
 export ssh_command="/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa  freddie@kirby.ecohealthalliance.org "
 
 #Pull latest version of master
-$ssh_command "cd /home/freddie/infrastructure/ && git pull"
+$ssh_command "cd /home/freddie/infrastructure/ && git pull" &&\
 
 #Build latest grits image
-$ssh_command "sudo docker build --no-cache -t grits /home/freddie/infrastructure/docker/images/grits/"
+$ssh_command "sudo docker build --no-cache -t grits /home/freddie/infrastructure/docker/images/grits/" &&\
 
 #Spin up latest celery worker
-$ssh_command "sudo docker-compose -f /home/freddie/infrastructure/docker/containers/grits-worker.yml up -d"
+$ssh_command "sudo docker-compose -f /home/freddie/infrastructure/docker/containers/grits-worker.yml up -d" &&\
 
 #Clean up to save on disk space
 $ssh_command "echo 'y'|sudo docker system prune"
