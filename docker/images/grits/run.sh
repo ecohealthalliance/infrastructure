295dd01a50db22ae2fd587ef5e3a6d08ea7ea5d8
Nathan Breit <nabreit@gmail.com>
2017-06-19 16:46:49 +0800

    Load environment variables from shared folder

--- docker/images/grits/run.sh
+++ docker/images/grits/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Load environment variables from shared folder
+source /shared/docker.env
 # Try to download premade database
 wget -O $ANNOTATOR_DB_PATH https://s3.amazonaws.com/bsve-integration/annotator.sqlitedb
 (
