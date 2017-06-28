9310f3bc9cd21436c53dff23a741ef56c28010a2
Abe Miessler <miessler@ecohealthalliance.org>
2017-06-28 09:28:05 -0700

    copying settings file that contains SMTP values over

--- docker/images/flirt/flirt.sh
+++ docker/images/flirt/flirt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export METEOR_SETTINGS=$(cat /shared/settings-production.json)
+cp /shared/config.py /flirt-simulation-service/simulator/config.py
 node /flirt-webapp/app/build/bundle/main.js 
 
