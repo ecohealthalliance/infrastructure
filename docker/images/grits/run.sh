70d9394e8e43bf68650a022ea84b613fecca97c1
Nathan Breit <nabreit@gmail.com>
2017-07-18 21:10:46 +0800

    Update importers script

--- docker/images/grits/run.sh
+++ docker/images/grits/run.sh
@@ -4,7 +4,6 @@ source /shared/docker.env
 # Try to download premade database
 wget -O $ANNOTATOR_DB_PATH https://s3.amazonaws.com/bsve-integration/annotator.sqlitedb
 (
-  $GRITS_HOME/grits_env/bin/python -m epitator.sqlite_import_disease_ontology &&
-  $GRITS_HOME/grits_env/bin/python -m epitator.sqlite_import_geonames &&
+  $GRITS_HOME/grits_env/bin/python -m epitator.importers.import_all &&
   supervisord --nodaemon --config /etc/supervisor/supervisord.conf
 )
