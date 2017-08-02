82d07cbad12ea3215b0e2d4cf4c7edff711760c4
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-02 15:12:03 -0400

    Scripted kubectl installation

---
+++ docker/install-kubectl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Install kubectl on ubuntu
+
+#Download latest version
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+
+#Make binary executable
+chmod +x ./kubectl
+
+#Move binary to PATH
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+
