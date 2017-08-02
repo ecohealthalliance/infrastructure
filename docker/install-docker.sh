400e1a9c7314bd19d96efaf948ce83a43a6b1309
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-02 14:59:59 -0400

    Quick bash script to automate docker installation

---
+++ docker/install-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Install docker on ubuntu
+
+#Install necessary packages
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates curl software-properties-common
+
+#Install docker repo key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+#Add official docker repo
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
+#Install docker engine community edition
+sudo apt-get install docker-ce
+
+
