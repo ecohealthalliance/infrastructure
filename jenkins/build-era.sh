d74c517c7e994299bd24b4a5b1e109bfbd00c8fd
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-15 10:37:34 -0400

    Update to build era on jenkins

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #This is a jenkins script
-export remote_command="/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@era.eha.io "
 
-$remote_command "cd /opt/infrastructure/docker/images/era/era && /usr/bin/sudo git pull origin master" &&\
-$remote_command "/usr/bin/sudo docker build -t era /opt/infrastructure/docker/images/era"
+cd /opt/infrastructure/docker/images/era/era && /usr/bin/sudo git pull origin master &&\
+/usr/bin/sudo docker build -t era /opt/infrastructure/docker/images/era &&\
+/usr/bin/sudo docker save era /tmp/era.tar &&\
+aws s3 cp /tmp/era.tar s3://eha-docker-repo/era.tar &&\
+rm /tmp/era.tar
 
