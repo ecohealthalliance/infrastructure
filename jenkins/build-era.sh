0bd04e97555b50ba5e483a821dbd405a412a3745
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-15 14:45:15 -0400

    Update to build-era.sh

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -3,8 +3,8 @@
 
 cd /opt/infrastructure/docker/images/era/era &&\
 docker build -t era /opt/infrastructure/docker/images/era &&\
-docker save era /tmp/era.tar &&\
-zip -1 /tmp/era.tar &&\
+docker save era > /tmp/era.tar &&\
+gzip -1 /tmp/era.tar &&\
 aws s3 cp /tmp/era.tar.gz s3://eha-docker-repo/era.tar.gz &&\
 rm /tmp/era.tar*
 
