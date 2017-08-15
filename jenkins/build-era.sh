bad07187129df8a9a524ffd765d7d2c604ee9693
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-15 10:39:31 -0400

    Jenkins user does not require sudo for docker

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #This is a jenkins script
 
-cd /opt/infrastructure/docker/images/era/era && /usr/bin/sudo git pull origin master &&\
-/usr/bin/sudo docker build -t era /opt/infrastructure/docker/images/era &&\
-/usr/bin/sudo docker save era /tmp/era.tar &&\
+cd /opt/infrastructure/docker/images/era/era &&\
+docker build -t era /opt/infrastructure/docker/images/era &&\
+docker save era /tmp/era.tar &&\
 aws s3 cp /tmp/era.tar s3://eha-docker-repo/era.tar &&\
 rm /tmp/era.tar
 
