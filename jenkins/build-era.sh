a44d8a3db3cc59b2466a1221f2d2d9c84b6a2227
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-15 10:46:30 -0400

    Docker era image from repo

--- jenkins/build-era.sh
+++ jenkins/build-era.sh
@@ -4,6 +4,7 @@
 cd /opt/infrastructure/docker/images/era/era &&\
 docker build -t era /opt/infrastructure/docker/images/era &&\
 docker save era /tmp/era.tar &&\
-aws s3 cp /tmp/era.tar s3://eha-docker-repo/era.tar &&\
-rm /tmp/era.tar
+zip -1 /tmp/era.tar &&\
+aws s3 cp /tmp/era.tar.gz s3://eha-docker-repo/era.tar.gz &&\
+rm /tmp/era.tar*
 
--- jenkins/deploy-era.sh
+++ jenkins/deploy-era.sh
@@ -6,6 +6,12 @@
 export ssh_command="/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa  ubuntu@era.eha.io "
 function scp_file { /usr/bin/scp -i /var/lib/jenkins/.ssh/id_rsa $1 ubuntu@era.eha.io:/tmp/; }
 
+#Copy image to server
+$ssh_command "aws s3 cp s3://eha-docker-repo/era.tar.gz /tmp/era.tar.gz" &&\
+$ssh_command "gzip -d /tmp/era.tar.gz" &&\
+$ssh_command "sudo docker load < /tmp/era.tar" &&\
+$ssh_command "rm /tmp/era.tar*" &&\
+
 #Reprovision flirt container
 scp_file /opt/infrastructure/docker/containers/era.yml &&\
 $ssh_command "sudo docker-compose -f /tmp/era.yml up -d"
