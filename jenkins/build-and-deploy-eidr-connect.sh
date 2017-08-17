f9a7377527efaaa30960ff59bae786ecde962218
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-17 10:11:20 -0400

    update to eidr-c deployment process

--- jenkins/build-and-deploy-eidr-connect.sh
+++ jenkins/build-and-deploy-eidr-connect.sh
@@ -12,6 +12,17 @@ git rev-parse HEAD > revision.txt &&\
 #Build the new image
 docker build --no-cache -t eidr-connect /opt/eidr-connect &&\
 
+#Upload image to s3 bucket
+docker save eidr-connect > /tmp/eidr-connect.tar &&\
+gzip -1 /tmp/eidr-connect.tar &&\
+aws s3 cp /tmp/eidr-connect.tar s3://eha-docker-repo/eidr-connect.tar.gz &&\
+rm /tmp/eidr-connect.tar* &&\
+
+#Load image onto server
+$remote_command "aws s3 s3://eha-docker-repo/eidr-connect.tar.gz /tmp/eidr-connect.tar.gz" &&\
+$remote_command "gzip -d /tmp/eidr-connect.tar.gz" &&\
+$remote_command "sudo docker load < /tmp/eidr-connect.tar" &&\
+
 #Instantiate the new image
 $remote_command "cd /opt/eidr-connect && git pull" &&\
 $remote_command "sudo docker-compose -f /opt/eidr-connect/eidr-connect.yml up -d"
