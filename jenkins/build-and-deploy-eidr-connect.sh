ffa338bf2c3f0eae9ddd267c3252ab89ba5a5a10
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-17 09:46:07 -0400

    Build Eidr-C on jenkins

--- jenkins/build-and-deploy-eidr-connect.sh
+++ jenkins/build-and-deploy-eidr-connect.sh
@@ -4,19 +4,16 @@
 export remote_command="ssh -ti /var/lib/jenkins/.ssh/id_rsa ubuntu@eidr-connect.eha.io"
 
 #Make sure repo is up to date
-$remote_command "cd /opt/eidr-connect && git checkout release; git pull; git rev-parse HEAD > revision.txt" &&\
-
-#Free up resources before the build
-$remote_command "sudo docker kill eidr-connect.eha.io" &&\
-$remote_command "sudo docker rm eidr-connect.eha.io" &&\
+cd /opt/eidr-connect &&\
+git checkout release &&\
+git pull &&\
+git rev-parse HEAD > revision.txt &&\
 
 #Build the new image
-$remote_command "sudo docker build --no-cache -t eidr-connect /opt/eidr-connect" &&\
-
-#Forcefully remove previous running container
-$remote_command "sudo docker rm -f eidr-connect.eha.io || true" &&\
+docker build --no-cache -t eidr-connect /opt/eidr-connect &&\
 
 #Instantiate the new image
+$remote_command "cd /opt/eidr-connect && git pull" &&\
 $remote_command "sudo docker-compose -f /opt/eidr-connect/eidr-connect.yml up -d"
 
 
