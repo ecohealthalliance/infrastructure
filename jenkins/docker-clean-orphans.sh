1bda8ef7a802e2b7bbfd47e215552d85779e62dc
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-18 09:39:12 -0400

    Update to docker.clean-orphans.sh

--- jenkins/docker-clean-orphans.sh
+++ jenkins/docker-clean-orphans.sh
@@ -2,15 +2,6 @@
 # Purpose: Remove docker images labeled as "none"
 # Author: Freddie Rosario <rosario@ecohealthalliance.org>
 
-remove_stopped_containers="docker ps -a|egrep \"Exited|Created\"|awk '{print $1}'|xargs docker rm -f"
-remove_orphans="docker images|grep none|awk '{print $3}'|xargs docker rmi"
-sudo_remove_orphans="(sudo docker images|grep none|awk '{print $3}'|xargs sudo docker rmi) || true"
-
-cleanup(){
-  ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@$1 $sudo_remove_orphans
-}
-
-
 docker_servers=(
   eidr-connect.eha.io
   eidr-connect-bsve.eha.io
@@ -22,27 +13,11 @@ docker_servers=(
 
 #Cleanup jenkins itself
 echo "Cleaning Jenkins..."
-docker ps -a |egrep "Exited|Created"
-
-#Check for and remove stopped containers
-if [[ $? -eq 0 ]];then
-  docker ps -a|egrep "Exited|Created"|awk '{print $1}'|xargs docker rm -f
-fi
-
-#Check for none images
-docker images|grep none
-if [[ $? -eq 0 ]];then
-  docker images|grep none|awk '{print $3}'|xargs docker rmi
-fi
+echo "y" | docker system prune
 
 #SSH to each machine, and clean up
 for server in "${docker_servers[@]}"; do
   echo "Cleaning $server..."
-  cleanup $server;
+  echo "y" | sudo docker system prune
 done
-
-#NIAM access is a little different
-echo "Cleaing Niam..."
-ssh ubuntu@niam.eha.io $sudo_remove_orphans
-
 
