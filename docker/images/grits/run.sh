d4601ec9ca60ebf2f5105fd68af7f81e68836992
Nathan Breit <nath@nathanathan.com>
2017-10-17 23:42:17 +0000

    move config data to secure.yml

--- docker/images/grits/run.sh
+++ docker/images/grits/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Wait before launching anything incase the container is restarted to make a config change.
 sleep 12
-# Load environment variables from shared folder
-source /shared/docker.env
 # Try to download premade database
 if [ ! -f "/shared/.db_download_complete" ]; then
     wget -O $ANNOTATOR_DB_PATH https://s3.amazonaws.com/bsve-integration/annotator.sqlitedb &&
