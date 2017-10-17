7fa2e9ab1798798bdab27d4af6182ad614f73c19
Nathan Breit <nath@nathanathan.com>
2017-10-17 15:46:05 +0000

    update jenkins scripts

--- jenkins/build-and-deploy-eidr-connect.sh
+++ jenkins/build-and-deploy-eidr-connect.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
-#Note: This script is intended for jenkins.
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook provision-instance-and-build.yml \
+  --private-key ~/.keys/infrastructure.pem \
+  --extra-vars "image_name=eidr-connect"
 
-export remote_command="ssh -ti /var/lib/jenkins/.ssh/id_rsa ubuntu@eidr-connect.eha.io"
+if [ "$NOTIFY_BSVE" = true ]; then
+  echo "Notify BSVE to redeploy"
+  aws sns publish --topic-arn arn:aws:sns:us-east-1:789867670404:EHA-Git-Lambda --message '{"app":"eidr-connect"}' --profile bsve-user
+fi
 
-#Make sure repo is up to date
-cd /opt/eidr-connect &&\
-git checkout release &&\
-git pull &&\
-git rev-parse HEAD > revision.txt &&\
-
-#Build the new image
-docker build --no-cache -t eidr-connect /opt/eidr-connect &&\
-
-#Upload image to s3 bucket
-rm /tmp/eidr-connect.tar*
-docker save eidr-connect > /tmp/eidr-connect.tar &&\
-gzip -1 /tmp/eidr-connect.tar &&\
-aws s3 cp /tmp/eidr-connect.tar.gz s3://eha-docker-repo/eidr-connect.tar.gz &&\
-rm /tmp/eidr-connect.tar* &&\
-
-#Load image onto server
-$remote_command "rm /tmp/eidr-connect.tar*"
-$remote_command "aws s3 cp s3://eha-docker-repo/eidr-connect.tar.gz /tmp/eidr-connect.tar.gz" &&\
-$remote_command "gzip -d /tmp/eidr-connect.tar.gz" &&\
-$remote_command "echo 'y' | sudo docker system prune" &&\
-$remote_command "sudo docker load < /tmp/eidr-connect.tar && rm /tmp/eidr-connect.tar*" &&\
-
-#Instantiate the new image
-$remote_command "cd /opt/eidr-connect && git pull" &&\
-$remote_command "sudo docker-compose -f /opt/eidr-connect/eidr-connect.yml up -d"
-
-
+ansible-playbook deploy-apps.yml \
+  --private-key ~/.ssh/id_rsa \
+  --tags deploy-eidr-connect
--- jenkins/build-and-deploy-grits.sh
+++ jenkins/build-and-deploy-grits.sh
@@ -1,67 +1,14 @@
 #!/bin/bash
-
-#Make sure repo is up to date
-cd /opt/infrastructure && git pull &&\
-
-#Build the new image
-docker build --no-cache -t grits /opt/infrastructure/docker/images/grits &&\
-
-#Upload new docker image to S3
-rm /tmp/grits.tar*
-docker save grits > /tmp/grits.tar &&\
-gzip -1 /tmp/grits.tar &&\
-aws s3 cp /tmp/grits.tar.gz s3://bsve-integration/grits.tar.gz &&\
-aws s3 cp /tmp/grits.tar.gz s3://eha-docker-repo/grits.tar.gz &&\
-echo "Images uploaded to s3 buckets" &&\
-rm /tmp/grits.tar.gz
-
-(
-/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa  ubuntu@grits.eha.io <<EOF
-#Load image onto grits server
-sudo docker system prune -f &&\
-aws s3 cp s3://eha-docker-repo/grits.tar.gz /tmp/grits.tar.gz &&\
-gzip -d /tmp/grits.tar.gz && echo 'Image decompressed' &&\
-sudo docker load < /tmp/grits.tar && echo 'Image loaded' &&\
-rm /tmp/grits.tar* &&\
-
-#Load geonames api image and data
-aws s3 cp s3://bsve-integration/elasticsearch-data.tar.gz /tmp/elasticsearch-data.tar.gz &&\
-aws s3 cp s3://bsve-integration/geonames-api.tar.gz /tmp/geonames-api.tar.gz &&\
-#Remove old elasticsearch data and ignore errors if it doesn't exist.
-sudo docker stop elasticsearch ; true &&\
-sudo rm -rf /mnt/elasticsearch ; true &&\
-sudo tar -xvzf /tmp/elasticsearch-data.tar.gz -C / &&\
-gzip -d /tmp/geonames-api.tar.gz &&\
-sudo docker load < /tmp/geonames-api.tar &&\
-rm /tmp/*.tar* ; true &&\
-
-#Instantiate the new image
-cd /opt/infrastructure && git pull
-EOF
-) &&\
-
-#Send super user commands to the server
-(
-/usr/bin/ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@grits.eha.io <<EOF
-sudo su
-(
-elasticsearch_data_path=/mnt/elasticsearch/data  \
-ip_address=\$(ip -4 route get 8.8.8.8 | awk '{print \$7}')  \
-docker-compose -f /opt/infrastructure/docker/containers/grits.yml up -d
-) &&\
-
-#Make sure grits has classifiers and disease lables
-docker cp /home/ubuntu/source-vars.sh.backup grits:/source-vars.sh &&\
-docker exec grits bash -c 'source /source-vars.sh && /scripts/update-settings.sh && /scripts/disease-label-autocomplete.sh' &&\
-
-#Restart grits
-docker kill grits && docker start grits &&\
-sleep 10 &&\
-docker exec grits supervisorctl start all
-EOF
-) &&\
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook provision-instance-and-build.yml \
+  --private-key ~/.keys/infrastructure.pem \
+  --extra-vars "image_name=grits"
 
 if [ "$NOTIFY_BSVE" = true ]; then
-  /bin/echo "Notify BSVE to redeploy grits"
+  /bin/echo "Notify BSVE to redeploy"
   aws sns publish --topic-arn arn:aws:sns:us-east-1:789867670404:EHA-Git-Lambda --message '{"app":"grits"}' --profile bsve-user
 fi
+
+ansible-playbook deploy-apps.yml \
+  --private-key ~/.ssh/id_rsa \
+  --tags deploy-grits
+
