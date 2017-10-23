7f206f15bb294189172752b666f23fc8c3c806ab
Nathan Breit <nath@nathanathan.com>
2017-10-23 16:20:07 +0000

    fix eidr-connect backups

--- jenkins/backup-eidr-connect.sh
+++ jenkins/backup-eidr-connect.sh
@@ -4,7 +4,7 @@
 export remote_command="ssh -ti /var/lib/jenkins/.ssh/id_rsa ubuntu@eidr-connect.eha.io"
 
 export DATE=$(date +%Y-%m-%d)
-$remote_command "sudo docker exec -t mongodb mongodump --out /var/log/dump" &&\
-$remote_command "sudo tar -czf /var/log/mongodump-$DATE.tar.gz /var/log/dump" &&\
-$remote_command "aws s3 cp /var/log/mongodump-$DATE.tar.gz s3://eidr-connect-backups"
-
+$remote_command "mkdir /tmp/ec-dumps"
+$remote_command "mongodump --port 27017 --out /tmp/ec-dumps/dump" &&\
+$remote_command "sudo tar -czf /tmp/ec-dumps/mongodump-$DATE.tar.gz /tmp/ec-dumps/dump" &&\
+$remote_command "aws s3 cp /tmp/ec-dumps/mongodump-$DATE.tar.gz s3://eidr-connect-backups"
