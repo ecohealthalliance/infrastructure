fa8c5a489df1642ed236ece5afaf766e5c5e539d
Nathan Breit <nath@nathanathan.com>
2017-12-21 17:03:16 +0000

    remove deprecated flirt init script

--- docker/images/flirt/flirt.sh
+++ docker/images/flirt/flirt.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 export METEOR_SETTINGS=$(cat /shared/settings-production.json)
-cp /shared/config.py /flirt-simulation-service/simulator/config.py
 node /flirt-webapp/app/build/bundle/main.js 
-
