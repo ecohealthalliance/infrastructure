f432116876519232f73d1562ef14fb756f29d6ed
Nathan Breit <nath@nathanathan.com>
2018-06-21 09:32:53 -0700

    Fix env. var loading bug

--- docker/images/promed-scraper/cronjob.sh
+++ docker/images/promed-scraper/cronjob.sh
@@ -1,4 +1,6 @@
+set -o allexport
+source /docker.env
+set +o allexport
 echo "Print out variables for testing"
 env
-source /docker.env
 /usr/local/bin/ansible-playbook --connection=local /promed_mail_scraper/ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" $OTHER_ANSIBLE_PARAMS
