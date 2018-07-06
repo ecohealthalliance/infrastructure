7ea6bce6c87bc314c1cb01d7bb438dde2a9c0c96
Nathan Breit <nath@nathanathan.com>
2018-07-06 22:58:29 +0000

    configure httpd

--- docker/images/wabnet/run.sh
+++ docker/images/wabnet/run.sh
@@ -3,5 +3,6 @@ set -e
 cd /wab-net-website
 /venv/bin/python manage.py makemigrations wabnet
 /venv/bin/python manage.py migrate
+/venv/bin/python manage.py collectstatic
 /venv/bin/python import_from_epicollect.py
 /venv/bin/python manage.py runserver
