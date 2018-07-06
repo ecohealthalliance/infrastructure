610963abc5af50cc5b07e557dc5c4439d70ceb2e
Nathan Breit <nath@nathanathan.com>
2018-07-06 23:09:58 +0000

    prevent collectstatic prompt

--- docker/images/wabnet/run.sh
+++ docker/images/wabnet/run.sh
@@ -4,6 +4,6 @@ env
 cd /wab-net-website
 /venv/bin/python manage.py makemigrations wabnet
 /venv/bin/python manage.py migrate
-/venv/bin/python manage.py collectstatic
 /venv/bin/python import_from_epicollect.py
+/venv/bin/python manage.py collectstatic --noinput
 /venv/bin/python manage.py runserver
