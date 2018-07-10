774e95b7a21ce00343191f91b117631fba5e5a62
Nathan Breit <nath@nathanathan.com>
2018-07-10 20:29:59 +0000

    add wabnet sync job

--- docker/images/wabnet/run.sh
+++ docker/images/wabnet/run.sh
@@ -4,6 +4,6 @@ env
 cd /wab-net-website
 /venv/bin/python manage.py makemigrations wabnet
 /venv/bin/python manage.py migrate
-/venv/bin/python import_from_epicollect.py
+/venv/bin/python manage.py import_from_epicollect wabnet.ec5_models
 /venv/bin/python manage.py collectstatic --noinput
 /venv/bin/python manage.py runserver 0.0.0.0:80
