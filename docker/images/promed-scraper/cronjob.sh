0139842c3708b7c6d47e304c0b00eb0a4ae71059
Toph Allen <toph.allen@gmail.com>
2019-12-10 12:03:19 -0500

    Fixed it???

--- docker/images/promed-scraper/cronjob.sh
+++ docker/images/promed-scraper/cronjob.sh
@@ -3,4 +3,4 @@ source /docker.env
 set +o allexport
 echo "Print out variables for testing"
 env
-/usr/local/bin/ansible-playbook --connection=local /promed_mail_scraper/ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" $OTHER_ANSIBLE_PARAMS
+ansible-playbook --connection=local /promed_mail_scraper/ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" $OTHER_ANSIBLE_PARAMS
