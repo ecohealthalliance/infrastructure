8e6305cf91d04d4af7c2da98f5e74f4425df5e3e
Toph Allen <toph.allen@gmail.com>
2019-12-10 14:34:57 -0500

    Reverting last change.

--- docker/images/promed-scraper/cronjob.sh
+++ docker/images/promed-scraper/cronjob.sh
@@ -3,4 +3,4 @@ source /docker.env
 set +o allexport
 echo "Print out variables for testing"
 env
-ansible-playbook --connection=local /promed_mail_scraper/ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" --tags=promed,download-classifier,s3backup --skip-tags=preloadable
+ansible-playbook --connection=local /promed_mail_scraper/ansible/site.yml --extra-vars "mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL" $OTHER_ANSIBLE_PARAMS
